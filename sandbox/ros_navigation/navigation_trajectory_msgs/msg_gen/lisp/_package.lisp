783168e976238a2e4f8c846895f80a75f06494f7
Alexey Zakharov <alexey.zakharov@gmail.com>
2012-06-26 17:23:17 +0200

    cleaning up

--- sandbox/ros_navigation/navigation_trajectory_msgs/msg_gen/lisp/_package.lisp
+++
@@ -1,7 +0,0 @@
-(cl:defpackage navigation_trajectory_msgs-msg
-  (:use )
-  (:export
-   "<TRAJECTORY>"
-   "TRAJECTORY"
-  ))
-
--- sandbox/ros_navigation/navigation_trajectory_msgs/msg_gen/lisp/_package_Trajectory.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package navigation_trajectory_msgs-msg)
-(cl:export '(HEADER-VAL
-          HEADER
-          TRAJECTORY-VAL
-          TRAJECTORY
-))
